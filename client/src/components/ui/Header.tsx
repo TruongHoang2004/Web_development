516a1661218de07964fd3d2eb4a273f59884a1d0
Mai Tien Huy <dnimer211@gmail.com>
2024-09-29 23:19:05 +0700

    them login va register va sua header

--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -18,13 +18,13 @@ const Header: React.FC = () => {
         <header className="border-b border-slate-300 text-green-950 py-4">
             <div className="w-10/12 m-auto flex items-center justify-between">
                 <h1 className={`${lobster.className} text-5xl mr-9`}>bookingLabour</h1>
-                <div className = {`${kanit.className} text-lg  font-normal flex h-5 items-center space-x-4`}>
+                <div className={`${kanit.className} text-lg  font-normal flex h-5 items-center space-x-4`}>
                     <a href="" className="no-underline">Services</a>
-                    <Separator orientation="vertical" className="bg-zinc-950 w-px"/>
-                    <a href="/login" className="no-underline">Sign-up</a>
-                    <Separator orientation="vertical" className="bg-zinc-950 w-px"/>
+                    <Separator orientation="vertical" className="bg-zinc-950 w-px" />
+                    <a href="/register" className="no-underline">Sign-up</a>
+                    <Separator orientation="vertical" className="bg-zinc-950 w-px" />
                     <a href="/login" className="no-underline">Login</a>
-                    <Separator orientation="vertical" className="bg-zinc-950 w-px"/>
+                    <Separator orientation="vertical" className="bg-zinc-950 w-px" />
                     <a href="" className="no-underline rounded-xl border border-lime-500 py-1 px-3 font-medium">Register as a Tasker</a>
                 </div>
             </div>
