e2e54dbbfbcdf95865cbc92407ad369ecd616a74
Mai Tien Huy <dnimer211@gmail.com>
2024-10-17 11:06:54 +0700

    initial commit

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,74 +1,68 @@
 'use client'
-import { Button, Col, Divider, Form, Input, Row } from 'antd';
-import { ArrowLeftOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 
 const Login = () => {
-
-  const onFinish = async (values: any) => {
-
-  };
-
   return (
-    <Row justify={"center"} style={{ marginTop: "30px" }}>
-      <Col xs={24} md={16} lg={8}>
-        <fieldset style={{
-          padding: "15px",
-          margin: "5px",
-          border: "1px solid #ccc",
-          borderRadius: "5px"
-        }}>
-          <legend>Đăng Nhập</legend>
-          <Form
-            name="basic"
-            onFinish={onFinish}
-            autoComplete="off"
-            layout='vertical'
-          >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your email!',
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-
-
-            <Form.Item
-            >
-              <Button type="primary" htmlType="submit">
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-          <Link href={"/"}><ArrowLeftOutlined /> Quay lại trang chủ</Link>
-          <Divider />
-          <div style={{ textAlign: "center" }}>
-            Chưa có tài khoản? <Link href={"/register"}>Đăng ký tại đây</Link>
+    <div className="flex justify-center items-center font-[sans-serif] h-full min-h-screen p-4"
+      style={{ backgroundImage: 'url(/qminhancut.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+      <div className="max-w-md w-full mx-auto">
+        <form className="bg-opacity-70 bg-white rounded-2xl p-6 shadow-[0_2px_16px_-3px_rgba(6,81,237,0.3)]">
+          <div className="mb-12">
+            <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
           </div>
-        </fieldset>
-      </Col>
-    </Row>
-  )
-}
+
+          <div>
+            <div className="relative flex items-center">
+              <input name="email" type="text" required
+                className="bg-transparent w-full text-sm text-gray-800 border-b border-gray-400 focus:border-gray-800 px-2 py-3 outline-none placeholder:text-gray-800"
+                placeholder="Enter email" />
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <div className="relative flex items-center">
+              <input name="password" type="password" required
+                className="bg-transparent w-full text-sm text-gray-800 border-b border-gray-400 focus:border-gray-800 px-2 py-3 outline-none placeholder:text-gray-800"
+                placeholder="Enter password" />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
+            <div className="flex items-center">
+              <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 border-gray-300 rounded" />
+              <label htmlFor="remember-me" className="ml-3 block text-sm text-gray-800">
+                Remember me
+              </label>
+            </div>
+            <div>
+              <a href="javascript:void(0);" className="text-blue-600 text-sm font-semibold hover:underline">
+                Forgot Password?
+              </a>
+            </div>
+          </div>
+
+          <Link href="/" className="flex items-center" style={{ transform: 'translate(-5px, 20px)' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <span className="ml-2">Quay lại trang chủ</span>
+          </Link>
+
+          <div className="mt-12">
+            <button type="button"
+              className="w-full py-2.5 px-4 text-sm font-semibold tracking-wider rounded-full text-white bg-gray-800 hover:bg-[#222] focus:outline-none">
+              Sign-in
+            </button>
+            <p className="text-gray-800 text-sm text-center mt-6">Don't have an account
+              <Link href="/register" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</Link>
+            </p>
+          </div>
+
+          <hr className="my-6 border-gray-400" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,6 @@
 'use client'
 import styles from '../styles/home_page.module.css';
 import Header from '../components/ui/Header';
-import Footer from '../components/ui/Footer'
 import Image from 'next/image';
 import { Input } from "@/components/ui/input"
 import { Button } from '@/components/ui/button';
@@ -14,21 +13,10 @@ import { PiPaintRollerDuotone } from "react-icons/pi";
 import { PiFireDuotone } from "react-icons/pi";
 import { useState } from 'react';
 import { IoMdCheckmark } from "react-icons/io";
-import { BiSolidCheckCircle } from "react-icons/bi";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-import { AiOutlinePlus } from "react-icons/ai";
-import { FaStar } from "react-icons/fa";
-import { BiSolidLeftArrow } from "react-icons/bi";
-import { BiSolidRightArrow } from "react-icons/bi";
 
 export default function Home() {
   const [activeOption, setActiveOption] = useState(1);
-  const handleClickOptions = (divid: number) => {
+  const handleClickOptions = (divid) => {
     setActiveOption(divid);
   };
 
@@ -86,9 +74,9 @@ export default function Home() {
         <Image src="/img/home_img/home_page_img_4.png" alt="Image 4" width={400} height={100} className='absolute top-0 right-0'/>
         <h2 className='text-green-900 text-5xl w-6/12 text-center absolute bottom-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-extrabold'>Schedule reliable assistance for household chores</h2>
         <div className='absolute top-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-16'>
-          <form action="" className='flex align-center'>
+          <form action="">
               <Input type="text" placeholder="What kind of task do you need help with?" className='focus:outline-none text-base font-semibold rounded-r-none inline w-4/5 border-green-900 border-2 py-6 px-4'/>
-              <Button type='submit' className='text-base py-6 px-6 rounded-l-none border-green-900 border-2 border-l-0 bg-lime-600 w-1/5 text-lg font-bold'>Create Task</Button>
+              <Button type='submit' className='text-base py-6 px-6 rounded-l-none border-green-900 border-2 border-l-0 bg-lime-600'>SEARCH</Button>
           </form>
         </div>
       </div>
@@ -131,161 +119,26 @@ export default function Home() {
         </div>
       </div>
       
-      <div className='w-full relative'>
-        <Image src="/img/home_img/home_page_img_8.png" alt="Image 4" width={120} height={200} className='absolute top-0 right-0'/>
-        <div className= {`w-8/12 m-auto mt-8 ${options_details[activeOption - 1].bg_color} rounded-2xl p-8 pl-20 pr-4`}>
-          <div className='w-11/12 m-auto relative'>
-            <img src={options_details[activeOption -  1].bg_img_src} alt="" className={`w-full object-cover rounded-xl ${styles.options_details_h}`}/>
-            <div className='rounded-xl absolute bg-slate-100 w-80 h-72 top-1/2 left-24 transform -translate-x-1/2 -translate-y-1/2'>
-              <div className='ml-8 mt-8 mr-4'>
-                <p className='text-3xl font-bold mb-6'>{options_details[activeOption - 1].title}</p>
-                <div className='flex items-start mb-6'>
-                  <IoMdCheckmark size={50} className='mr-4 pb-6'/>
-                  <p className='font-medium'>{options_details[activeOption - 1].description1}</p>
-                </div>
-                <div className='flex items-start'>
-                  <IoMdCheckmark size={50} className='mr-4 pb-6'/>
-                  <p className='font-medium'>{options_details[activeOption - 1].description2}</p>
-                </div>
+      <div className= {`w-8/12 m-auto mt-8 ${options_details[activeOption - 1].bg_color} rounded-2xl p-8 pl-20 pr-4`}>
+        <div className='w-11/12 m-auto relative'>
+          <img src={options_details[activeOption -  1].bg_img_src} alt="" className={`w-full object-cover rounded-xl ${styles.options_details_h}`}/>
+          <div className='rounded-xl absolute bg-slate-100 w-80 h-72 top-1/2 left-24 transform -translate-x-1/2 -translate-y-1/2'>
+            <div className='ml-8 mt-8 mr-4'>
+              <p className='text-3xl font-bold mb-6'>{options_details[activeOption - 1].title}</p>
+              <div className='flex items-start mb-6'>
+                <IoMdCheckmark size={50} className='mr-4 pb-6'/>
+                <p className='font-medium'>{options_details[activeOption - 1].description1}</p>
+              </div>
+              <div className='flex items-start'>
+                <IoMdCheckmark size={50} className='mr-4 pb-6'/>
+                <p className='font-medium'>{options_details[activeOption - 1].description2}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='w-8/12 m-auto mt-24 flex justify-center'>
-        <p className='text-4xl font-bold border-b-2 border-zinc-200 pb-2 px-10'>GET STARTED NOW</p>
-      </div>
-      <div className='w-full relative'>
-        <Image src="/img/home_img/home_page_img_1.png" alt="Image 3" width={100} height={200} className='absolute bottom-0 left-0'/>
-        <Image src="/img/home_img/home_page_img_6.png" alt="Image 4" width={120} height={200} className='absolute top-0 right-0'/>
-        <div className='w-8/12 m-auto mt-10 text-zinc-800 flex flex-col justify-center gap-y-16'>
-          <div className='flex flex-wrap gap-x-16 justify-center gap-y-8 '>
-            <div className='w-96 h-80'>
-              <img src="/img/home_img/booking.jpg" alt='bg' className='w-full h-full object-cover rounded-xl'/>
-            </div>
-            <div className='w-96'>
-              <p className='font-bold text-4xl mb-4'>Book trusted help for home task</p>
-              <p className='font-bold text-gray-400 text-lg mb-4'>Get matched with top-rated professionals for all your needs</p>
-              <Button className='mr-4 font-bold'>Book now</Button>
-              <Button className='font-extrabold bg-gray-300 text-black'>Learn more</Button>
-            </div>
-          </div>
-          <div className='flex flex-wrap gap-x-16 justify-center gap-y-8'>
-            <div className='w-96'>
-              <p className='font-bold text-4xl mb-4'>Become a versatile Home Task Experts</p>
-              <p className='font-bold text-gray-400 text-lg mb-4'>Bring expertise, tools, and a can-do attitude to make sure customer's project is completed efficiently.</p>
-              <Button className='mr-4 font-bold'>Start working now</Button>
-              <Button className='font-extrabold bg-gray-300 text-black'>Learn more</Button>
-            </div>
-            <div className='w-96 h-80'>
-              <img src="/img/home_img/working.jpg" alt='bg' className='w-full h-full object-cover rounded-xl object-right'/>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className='bg-gradient-to-b from-emerald-800 via-emerald-900 to-zinc-950 text-white w-full mt-24 py-12'>
-        <div className='w-8/12 m-auto flex justify-center mb-16'>
-          <p className='text-4xl font-bold border-b-2 border-zinc-600 pb-2 px-10'>PRICING and POLICIES</p>
-        </div>
-        <div className='w-10/12 m-auto flex flex-wrap gap-x-10 justify-center gap-y-10'>
-            <div className='rounded-xl bg-slate-50 text-slate-900 p-10 w-80 hover:bg-gray-300'>
-              <p className='text-xl font-bold text-gray-500 mb-4'>Personal</p>
-              <p className='text-6xl font-bold mb-10'>$5 <span className='text-lg text-gray-500'>p/transaction</span></p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'> <BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <Button className='mt-10 bg-zinc-800 font-bold text-lg'>Get started</Button>
-            </div>
-            <div className='rounded-xl bg-slate-50 text-slate-900 p-10 w-80 hover:bg-gray-300'>
-              <p className='text-xl font-bold text-gray-500 mb-4'>Personal</p>
-              <p className='text-6xl font-bold mb-10'>$5 <span className='text-lg text-gray-500'>p/transaction</span></p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'> <BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <Button className='mt-10 bg-zinc-800 font-bold text-lg'>Get started</Button>
-            </div>
-            <div className='rounded-xl bg-slate-50 text-slate-900 p-10 w-80 hover:bg-gray-300'>
-              <p className='text-xl font-bold text-gray-500 mb-4'>Personal</p>
-              <p className='text-6xl font-bold mb-10'>$5 <span className='text-lg text-gray-500'>p/transaction</span></p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <p className='text-base mb-2 font-semibold'><BiSolidCheckCircle size={22} className='inline mr-4'/>Guanrantee completion</p>
-              <Button className='mt-10 bg-zinc-800 font-bold text-lg'>Get started</Button>
-            </div>
-        </div>
-        
-        <div className='w-8/12 m-auto flex justify-center mb-8 mt-20'>
-          <p className='text-4xl font-bold border-b-2 border-zinc-600 pb-2 px-10'>REVIEWS</p>
-        </div>
-        <div className='w-8/12 m-auto flex flex-col justify-center gap-y-6 bg-white text-black py-6 rounded-3xl px-8'>
-          <p className='text-gray-700 font-medium text-lg text-center'>
-            “Thank you for building such an empowering tool, especially for designers! 
-            My job was finished in less than a day. Great!
-            Thank you for building such an empowering tool, especially for designers! 
-            My job was finished in less than a day. Great!”
-          </p>
-          <div className='flex justify-center'> 
-            <FaStar className='w-14 h-14 text-yellow-300 mr-2'/>
-            <FaStar className='w-14 h-14 text-yellow-300 mr-2'/>
-            <FaStar className='w-14 h-14 text-yellow-300 mr-2'/>
-            <FaStar className='w-14 h-14 text-yellow-300 mr-2 '/>
-            <FaStar className='w-14 h-14 text-yellow-300 mr-2'/>
-          </div>
-          <div className='flex justify-between items-center'>
-            <BiSolidLeftArrow className='w-16 h-16'/>
-            <div className='flex items-center justify-center gap-x-3'>
-              <img src="/img/home_img/profile.png" alt="" className='w-20 h-20'/>
-              <div>
-                <p className= {`text-xl font-semibold ${styles.mb_2}`}>Karla Ray</p>
-                <p className='text-sm font-semibold text-gray-500'>@kayray</p>
-              </div>
-            </div>
-            <BiSolidRightArrow className='w-16 h-16'/>
-          </div>
-        </div>
-
-        <div className='w-8/12 m-auto flex justify-center mb-8 mt-20'>
-          <p className='text-4xl font-bold border-b-2 border-zinc-600 pb-2 px-10'>FAQ</p>
-        </div>
-        <div className='w-8/12 m-auto p-8 rounded-xl bg-white text-gray-500 pb-12'>
-          <Accordion type="single" collapsible className=''>
-            <AccordionItem value="item-1">
-            <AccordionTrigger className='text-xl font-semibold text-left'>
-              <div className='flex'><AiOutlinePlus className='inline-block mt-1 mr-2'/> Is it accessible?</div>
-            </AccordionTrigger>
-            <AccordionContent className='text-lg font-bold text-zinc-800'>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-2">
-            <AccordionTrigger className='text-xl font-semibold'>
-              <div className='flex'><AiOutlinePlus className='inline-block mt-1 mr-2'/> Is it accessible?</div>
-            </AccordionTrigger>
-            <AccordionContent className='text-lg font-bold text-zinc-800'>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-3">
-            <AccordionTrigger className='text-xl font-semibold'>
-              <div className='flex'><AiOutlinePlus className='inline-block mt-1 mr-2'/> Is it accessible?</div>
-            </AccordionTrigger>
-            <AccordionContent className='text-lg font-bold text-zinc-800'>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </div>
-       
-      </div>
-      <Footer/>
+    
     </div>
   );
 }
